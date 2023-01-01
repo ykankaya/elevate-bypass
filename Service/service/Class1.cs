1f4a54235af86b7d3cacb7007156b6347c5e3ef0
H4de5 <48757788+H4de5-7@users.noreply.github.com>
2023-01-01 15:09:44 +0800

    Add files via upload

---
+++ Service/service/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceProcess;
+
+namespace service
+{
+    internal class Class1
+    {
+        public static string p = "flag";
+        static void Main(string[] args)
+        {
+            p = args[0];
+            svc1.RunService();
+        }
+
+    }
+
+    public partial class svc1 : ServiceBase
+    {
+        public static void RunService()
+        {
+            Run(new svc1());
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            System.Diagnostics.Process.Start(Class1.p);
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
